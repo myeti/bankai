fd9bca1e816079d57bdecc355937f710b83448d6
Aymeric <aassier@wideagency.com>
2020-03-17 22:13:06 +0100

    Bump version

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import { formatDate } from './utils'
+import pkg from '../package.json'
 import './worker'
 
-Vue.config.productionTip = false
+console.log(`Bankai v${pkg.version}`)
 
+Vue.config.productionTip = false
 Vue.filter('date', formatDate)
 
 new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
